75b1d5d11126424e0fd5e23d26637fa1e90ad3ed
Ethan-Edgerton <Ethan.E.Edgerton@gmail.com>
2020-03-09 20:01:45 -0500

    Added comments for future reference

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-const StyledDiv = styled.div `
-background: white;
-margin: 1rem;
-width: 15vw;
-border: 3px solid black;
-border-radius: .5rem;
+const StyledDiv = styled.div`
+  background: white;
+  margin: 1rem;
+  width: 15vw;
+  border: 3px solid black;
+  border-radius: 0.5rem;
 `;
 
-const StyledName = styled.h1 `
-text-shadow: 1px 1px 5px rgb(236, 33, 33);
+const StyledName = styled.h1`
+  text-shadow: 1px 1px 5px rgb(236, 33, 33);
 `;
 
-
-const CharacterComponent = (props) => {
-   return (
+const CharacterComponent = props => {
+  return (
+    // props = "character" from {chars.map(character => {} from App.js
     <StyledDiv key={props.id}>
-        <StyledName>{props.name}</StyledName>
-        <p>Height: {props.height}</p>
-        <p>Mass: {props.mass}</p>
-        <p>Hair Color: {props.hairColor}</p>
-        <p>Skin Color: {props.skinColor}</p>
-        <p>Eye Color: {props.eyeColor}</p>
-        <p>Year of Birth: {props.birthYear}</p>
-        <p>Gender: {props.gender}</p>
+      <StyledName>{props.name}</StyledName>
+      <p>Height: {props.height} cm</p>
+      <p>Mass: {props.mass} Kg</p>
+      <p>Hair Color: {props.hairColor}</p>
+      <p>Skin Color: {props.skinColor}</p>
+      <p>Eye Color: {props.eyeColor}</p>
+      <p>Year of Birth: {props.birthYear}</p>
+      <p>Gender: {props.gender}</p>
+  {// <p>ExampleText: {props.exampleData}
+  }
     </StyledDiv>
-    )
-}
+  );
+};
 
 export default CharacterComponent;
